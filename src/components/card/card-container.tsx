7487c2c89b26bc84f1c1bde0e608d7da19435fa2
Dylan Connolly <dylanconnolly145@gmail.com>
2023-03-31 19:09:18 -0600

    switch components to typescript

--- src/components/card/card-container.tsx
+++ src/components/card/card-container.tsx
@@ -1,6 +1,6 @@
 import styles from './card-container.module.css';
 
-export default function CardContainer({ children }) {
+export default function CardContainer({ children }: any) {
   return (
     <div className={styles.container}>
       {children}
--- src/components/card/card-footer.tsx
+++ src/components/card/card-footer.tsx
@@ -1,13 +1,16 @@
 import styles from './card-footer.module.css'
-import { useState } from 'react';
+import { useState, MouseEvent } from 'react';
 
 export default function CardFooter () {
   const [expanded, setExpanded] = useState(false);
 
-  const handleExpandClick = (event) => {
+  const handleExpandClick = (event: MouseEvent<HTMLButtonElement> ) => {
     setExpanded(!expanded)
     console.log(expanded)
-    event.currentTarget.style.transform = "rotate('180deg')"
+    
+    const target = event.target as Element;
+    target.classList.toggle('expand')
+
       // gets icon element as child of button
     // event.currentTarget.firstElementChild.classList.toggle('up')
   }
@@ -16,10 +19,10 @@ export default function CardFooter () {
     <>
     <div className={styles.container}>
       <button className={styles.expandMoreButton} onClick={handleExpandClick}>
-        <icon className={styles.expandMoreIcon}>
+        <div className={styles.expandMoreIcon}>
           random text{/* <span>&#8964;</span> */}
           {/* {expanded ? <span>&#8963;</span>: <span>&#8964;</span>} */}
-        </icon>
+        </div>
       </button>
     </div>
     { expanded ? <p>i'm expanded a bit weeee</p>: ''}
